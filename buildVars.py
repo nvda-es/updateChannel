fe9682c4cb8117f4e6945dbf76892b9e9d70bd0a
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2021-10-05 16:54:10 +0200

    Updated to version 1.1 stable

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""This add-on allows you to switch quickly between NVDA snapshots and release types. Go to the update channel category on the NVDA settings dialog, choose your update channel and check for updates from the NVDA help menu. Choose the stable option and update again to return to an official release."""),
 	# version
-	"addon_version" : "1.1-dev",
+	"addon_version" : "1.1",
 	# Author(s)
 	"addon_author" : "Jose Manuel Delicado <jm.delicado@nvda.es>, Javi Dominguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
@@ -32,7 +32,7 @@ addon_info = {
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion" : "2021.1.0",
 	# Add-on update channel (default is stable or None)
-	"addon_updateChannel" : "dev",
+	"addon_updateChannel" : None,
 }
 
 import os.path
